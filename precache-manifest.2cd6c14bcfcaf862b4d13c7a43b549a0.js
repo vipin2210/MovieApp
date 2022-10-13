03c0fd7bcb49bfcc8b6174c566aabf4a85bc4160
Vipin <2002vipin318@gmail.com>
2022-10-13 07:40:15 +0530

    Updates

---
+++ precache-manifest.2cd6c14bcfcaf862b4d13c7a43b549a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece1d159b64d2902e8ff",
+    "url": "/MovieApp/static/css/main.1a18dffd.chunk.css"
+  },
+  {
+    "revision": "ece1d159b64d2902e8ff",
+    "url": "/MovieApp/static/js/main.ece1d159.chunk.js"
+  },
+  {
+    "revision": "527b60b4d89fe87ec31e",
+    "url": "/MovieApp/static/js/1.527b60b4.chunk.js"
+  },
+  {
+    "revision": "5dfb1f54ad9e9ba2849e",
+    "url": "/MovieApp/static/js/runtime~main.5dfb1f54.js"
+  },
+  {
+    "revision": "c40659eff26d9e7f64d5963846be24aa",
+    "url": "/MovieApp/index.html"
+  }
+];
